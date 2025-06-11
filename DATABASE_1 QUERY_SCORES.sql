741a5984f3767577b46bbdbadf0babbc8038460e
Lee-Anne22 <leeannevdm6@gmail.com>
2025-06-11 19:57:49 +0200

    #DATABASE_1 TABLE INFORMATION

--- DATABASE_1 QUERY_SCORES.sql
+++ DATABASE_1 QUERY_SCORES.sql
@@ -1,28 +1,28 @@
---CREATE DATABASE DATABASE_1
---CREATE DATABASE DATABASE_2;
+CREATE DATABASE DATABASE_1
+CREATE DATABASE DATABASE_2;
 USE DATABASE_1
 -----------------------
---CREATE TABLE SUBJECT(
-   --  Subject_Code INT,
-   --  Subjcet_Name NVARCHAR(55),
-   --  Subject_Marks INT
---);
+CREATE TABLE SUBJECT(
+    Subject_Code INT,
+    Subjcet_Name NVARCHAR(55),
+    Subject_Marks INT
+);
 ----------------------------
---INSERT INTO Subject VALUES (501, 'SQL', 75);
---INSERT INTO Subject VALUES(502, 'Python', 60),
-                       --    (503, 'PowerBI',65),
-                       --    (504, 'JAVA', 80),
-                       --    (505, 'TABLEU', 90
- --);
---SELECT*FROM Subject
---ORDER BY Subject_Code ASC;
+INSERT INTO Subject VALUES (501, 'SQL', 75);
+INSERT INTO Subject VALUES(502, 'Python', 60),
+                         (503, 'PowerBI',65),
+                         (504, 'JAVA', 80),
+                         (505, 'TABLEU', 90
+ );
+SELECT*FROM Subject
+ORDER BY Subject_Code ASC;
 
 ------------------------------
---CREATE TABLE SUBJECT_SCORING (
-          --  SCORING_RATE INT,
-          --  SUBJECT_PRIZE INT,
-          --  SUBJECT_CODE INT
---);
+CREATE TABLE SUBJECT_SCORING (
+            SCORING_RATE INT,
+            SUBJECT_PRIZE INT,
+            SUBJECT_CODE INT
+);
 -------------------------------
 --INSERT INTO SUBJECT_SCORING VALUES (80, 70, 503),
                                   -- (90,66,501),
